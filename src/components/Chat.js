e48dd77fbe0989319f194439590cfeea36fbe456
Ryland Adams <rylandadams@yahoo.com>
2023-01-16 11:19:11 -0800

    signout and live chat working!

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,13 @@
-import { useState } from 'react';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import {
+	addDoc,
+	collection,
+	serverTimestamp,
+	onSnapshot,
+	query,
+	where,
+	orderBy,
+} from 'firebase/firestore';
 import { auth, db } from '../firebase-config';
 import '../styles/Chat.css';
 
@@ -7,8 +15,26 @@ export const Chat = (props) => {
 	const { room } = props;
 
 	const [newMessage, setNewMessage] = useState('');
+	const [messages, setMessages] = useState([]);
 
 	const messagesRef = collection(db, 'messages');
+
+	useEffect(() => {
+		const queryMessages = query(
+			messagesRef,
+			where('room', '==', room),
+			orderBy('createdAt')
+		);
+		const unsub = onSnapshot(queryMessages, (snapshot) => {
+			let messages = [];
+			snapshot.forEach((doc) => {
+				messages.push({ ...doc.data(), id: doc.id });
+			});
+			setMessages(messages);
+		});
+
+		return () => unsub();
+	}, []);
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -25,6 +51,20 @@ export const Chat = (props) => {
 	};
 	return (
 		<div className='chat-app'>
+			<div className='header'>
+				<h1> Welcome to: {room.toUpperCase()}</h1>
+			</div>
+			<div className='messages'>
+				{messages.map((message) => (
+					<div
+						className='message'
+						key={message.id}
+					>
+						<span className='user'>{message.user}</span>
+						{message.text}
+					</div>
+				))}
+			</div>
 			<form
 				className='new-message-form'
 				onSubmit={handleSubmit}
